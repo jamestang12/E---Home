cbab0eb9918323fbdfad44b3b1976132eca3dfe7
James Tang <jamestang@Jamess-MacBook-Pro.local>
2020-11-02 16:29:54 -0500

    Created Database Schemas and Setup DB With Django

--- eHome/settings.py
+++ eHome/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'eHome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'jamestang',
+        'PASSWORD': 'james123',
+        'HOST': 'localhost'
     }
 }
 
