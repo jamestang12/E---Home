453c6b09ab19ed1168acf070bb9e320413164985
James Tang <jamestang@Jamess-MacBook-Pro.local>
2020-11-19 17:05:30 -0500

    Contact Form & Inquiry Submission Created

--- contacts2/models.py
+++ contacts2/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from django.db import models
 from datetime import datetime
 
 class Contact(models.Model):
@@ -8,10 +10,7 @@ class Contact(models.Model):
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     message = models.TextField(blank=True)
-    contact_date = models.DurationField(default=datetime.now, blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
     def __str__(self):
         return self.name
-
-
-
